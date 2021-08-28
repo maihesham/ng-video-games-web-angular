90d466e05081c02c2732e64acac4bb13729f092b
maihesham <maihesham19@yahoo.com>
2021-08-28 13:22:14 -0700

    get games from api and display it

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { APIResponse, Game } from 'src/app/Moduls';
+import { HttpService } from 'src/app/Services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,13 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-public sort:string='';
-  constructor() { }
+
+
+  public sort: string='';
+  public games: Array<Game>=[];
+private gameSub: Subscription | undefined;
+private routeSub: Subscription | undefined;
+  constructor(
+    private httpService: HttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
   }
-  searchGames(sort:string){
-
+  searchGames(sort:string,search:string=''):void{
+    this.gameSub = this.httpService
+    .getGamesList(sort, search)
+    .subscribe((gameList: APIResponse<Game>) => {
+      this.games = gameList.results;
+      console.log(gameList);
+    });
   }
 
 }
