8240acf5718ca3ea135602ad6ae592c4f30e8224
maihesham <maihesham19@yahoo.com>
2021-08-28 12:54:05 -0700

    interceptor

--- src/app/Services/http.service.ts
+++ src/app/Services/http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {environment as env} from 'src/environments/environment'
+import { Observable } from 'rxjs';
 import { APIResponse, Game } from '../Moduls';
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,8 @@ export class HttpService {
   BASE_URL: string ='https://rawg-video-games-database.p.rapidapi.com'
 
   constructor(private http:HttpClient) { }
-  getGamesList(ordering:string,search:string)
-  
+  getGamesList(ordering:string,search:string):
+  Observable<APIResponse<Game>>
   {
      let params=new HttpParams().set('ordering',ordering);
      if(search){
