1541e102badf5ebb7e5c4364977dae0724252a1c
maihesham <maihesham19@yahoo.com>
2021-08-28 14:01:11 -0700

    add game-tab

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from 'src/app/Moduls';
@@ -9,7 +9,8 @@ import { HttpService } from 'src/app/Services/http.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit  , OnDestroy
+{
   gameRating:number = 0;
   gameId: string='';
   game: Game | any;
@@ -44,6 +45,15 @@ export class DetailsComponent implements OnInit {
       return '#f7aa38';
     } else {
       return '#ef4655';
+    }
+  }
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
     }
   }
 }
